8865ee7d1646d89e7fba13d29cbefd74f3ed5ffb
Shiyu Li <timlee0212@outlook.com>
2022-07-18 18:11:55 -0400

    Fix the failure of cacheline invalidation error.

--- tests/hello.c
+++ tests/hello.c
@@ -1,19 +1,23 @@
 #include <fcntl.h>
 #include <stdbool.h>
 #include <stdint.h>
+#include <malloc.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/ioctl.h>
 
 #include "pimio.h"
 
+#define TEST_SIZE 128
+#define CACHELINE_SIZE 64
+
 #define BOOLSTR(val) (val ? "true" : "false")
 
 int main(void) {
-    __uint8_t *srcdata = (__uint8_t *)malloc(512 * sizeof(__uint8_t));
-    for (int i = 0; i < 256; i++) {
+    __uint8_t *srcdata = (__uint8_t *)aligned_alloc(CACHELINE_SIZE, TEST_SIZE * 2 * sizeof(__uint8_t));
+    for (int i = 0; i < TEST_SIZE; i++) {
         srcdata[i] = i;
-        srcdata[256 + i] = 0;
+        srcdata[TEST_SIZE+i] = 0;
     }
 
     int pim_fd = open("/dev/pimio", O_RDWR);
@@ -21,6 +25,7 @@ int main(void) {
         printf("Cannot open device for use!\n");
         exit(-1);
     }
+
     struct pimio_csr_args csr;
     ioctl(pim_fd, PIMIO_IOCTL_STATUS, &csr);
     printf(
@@ -30,20 +35,23 @@ int main(void) {
 
     printf("=========== Test 1: Loop Back =============\n");
     struct pimio_transfer_args transfer = {
-        .memAddr = srcdata, .wlAddr = 0x10, .reqLen = 256, .writeOp = true};
+        .memAddr = srcdata, .wlAddr = 0x10, .reqLen = TEST_SIZE, .writeOp = true};
     ioctl(pim_fd, PIMIO_IOCTL_TRANSFER, &transfer);
     printf("Copy transfer done!\n");
 
-    transfer.memAddr += 256;
+    transfer.memAddr += TEST_SIZE;
     transfer.writeOp = false;
+    transfer.reqLen = TEST_SIZE;
     ioctl(pim_fd, PIMIO_IOCTL_TRANSFER, &transfer);
 
-    for (int i = 0; i < 256; i++) {
-        printf("Loop back at %d: %d\n", i, srcdata[256 + i]);
+
+    //Load the data back to program space memory
+    for (int i = 0; i < TEST_SIZE; i++) {
+        printf("Loop back at %d: %d\n", i, srcdata[TEST_SIZE + i]);
     }
 
     printf("=========== Test 2: Compute =============\n");
-    __uint8_t *result = (__uint8_t *)malloc(csr.blBytes * sizeof(__uint8_t));
+    __uint8_t *result = (__uint8_t *)aligned_alloc(CACHELINE_SIZE, csr.blBytes * sizeof(__uint8_t));
     struct pimio_compute_args comp = {
         .memAddr = result, .wlAddr = 0x10, .srcOp = 2};
     ioctl(pim_fd, PIMIO_IOCTL_COMPUTE, &comp);
